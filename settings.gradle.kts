fe3cd340be31a30b5f28b1002f6fd3eec06d7de5
jr. Ankit <codewithankit056@gmail.com>
2025-10-01 21:59:57 +0530

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        maven("https://jitpack.io")
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven("https://jitpack.io")
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Voice Recorder"
+include(":app")
+ 
